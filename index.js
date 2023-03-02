db5e2574ff50428d5dd490a498a2c42ec5845ce8
Digambaran <arjunsreekumar.1@gmail.com>
2023-03-02 10:02:06 +0530

    perf: better signup form

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ redis.on("connect", () => {
 
 env.init();
 
+console.log(process.env);
 const prisma = new PrismaClient({
   datasources: { db: { url: process.env.DATABASE_URL } },
   log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
